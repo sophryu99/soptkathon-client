5bc77c2ded9be3bf6eaddd63c6ac354f0bb356b2
jaeukyang <didwodnr123@gmail.com>
2020-06-07 07:38:28 +0900

    autoLayout

--- iOS-Soptkathon/iOS-Soptkathon/APIService/MusicServiceJU.swift
+++ iOS-Soptkathon/iOS-Soptkathon/APIService/MusicServiceJU.swift
@@ -8,4 +8,35 @@
 
 import Foundation
 import Alamofire
-
+//
+//struct MusicService {
+//    private init() { }
+//    static let shared = MusicService()
+//
+//    let header: HTTPHeaders = [
+//        "Content-Type" : "application/json"
+//    ]
+//
+//    func getMusicResult(text1: String, completion: @escaping (_ b: Bool) -> Void) {
+//
+//        let url = APIConstants.musicURL
+//
+//        Alamofire.request(url).responseJSON {
+//            response in
+//
+//            switch response.result {
+//            case .success(let value):
+//                let json = JSON(value)
+//                let result = json["result"].intValue
+//                if result == 404 {
+//                    completion(true)
+//                }else {
+//                    completion(false)
+//                }
+//
+//            default:
+//                return
+//            }
+//        }
+//    }
+//}
