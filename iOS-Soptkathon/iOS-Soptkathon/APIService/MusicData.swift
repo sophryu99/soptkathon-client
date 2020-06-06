92b5c6ea9bd50dd4c5e5809da1f71c6c85f26ca0
jaeukyang <didwodnr123@gmail.com>
2020-06-07 07:57:36 +0900

    머지 전 커밋

--- iOS-Soptkathon/iOS-Soptkathon/APIService/MusicData.swift
+++ iOS-Soptkathon/iOS-Soptkathon/APIService/MusicData.swift
@@ -8,28 +8,31 @@
 
 import Foundation
 
-//struct MusicData: Codable {
-//    var status: Int
-//    var success: Bool
-//    var message: String
-//    var data: MusicList?
-//    enum CodingKeys: String, CodingKey {
-//        case status = "status"
-//        case success = "success"
-//        case message = "message"
-//        case data = "data"
-//
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        status = (try? values.decode(Int.self, forKey: .status)) ?? -1 success = (try? values.decode(Bool.self, forKey: .success)) ?? false message = (try? values.decode(String.self, forKey: .message)) ?? "" data = (try? values.decode(TokenData.self, forKey: .data)) ?? nil
-//    }
-//
-//}
-//
-//struct MusicList: Codable{
-//    var idx: Int
-//    var title: String
-//    var
-//}
+struct MusicData: Codable {
+    var status: Int
+    var success: Bool
+    var message: String
+    var data: MusicList?
+    enum CodingKeys: String, CodingKey {
+        case status = "status"
+        case success = "success"
+        case message = "message"
+        case data = "data"
+
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = (try? values.decode(Int.self, forKey: .status)) ?? -1
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode(MusicList.self, forKey: .data)) ?? nil
+    }
+
+}
+
+struct MusicList: Codable{
+    var idx: Int
+    var title: String
+    var subTitle: String
+}
--- iOS-Soptkathon/iOS-Soptkathon/APIService/MusicServiceJU.swift
+++ iOS-Soptkathon/iOS-Soptkathon/APIService/MusicServiceJU.swift
@@ -20,23 +20,32 @@ import Alamofire
 //    func getMusicResult(text1: String, completion: @escaping (_ b: Bool) -> Void) {
 //
 //        let url = APIConstants.musicURL
-//
-//        Alamofire.request(url).responseJSON {
-//            response in
-//
-//            switch response.result {
-//            case .success(let value):
-//                let json = JSON(value)
-//                let result = json["result"].intValue
-//                if result == 404 {
-//                    completion(true)
-//                }else {
-//                    completion(false)
+//        
+//        func getData(completion: @escaping (NetworkResult<Any>)->Void){
+//            let dataRequest = Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header)
+//            
+//            dataRequest.responseData{ dataResponse in
+//                switch dataResponse.result{
+//                case .success:
+//                    guard let statusCode = dataResponse.response?.statusCode else { return }
+//                    guard let value = dataResponse.result.value else { return }
+//                    let networkResult = self.judge(by: statusCode, value)
+//                    completion(networkResult)
+//                case .failure: completion(.networkFail)
 //                }
-//
-//            default:
-//                return
 //            }
 //        }
+//        private func judge(by statusCode: Int, _ data: Data) -> NetworkResult<Any> { switch statusCode {
+//            case 200: return isUser(by: data)
+//            case 400: return .pathErr
+//            case 500: return .serverErr default: return .networkFail }
+//        }
 //    }
+//        private func isUser(by data: Data) -> NetworkResult<Any> {
+//        let decoder = JSONDecoder()
+//            guard let decodedData = try? decoder.decode(SigininData.self, from: data) else { return .pathErr }
+//            guard let tokenData = decodedData.data else { return .requestErr(decodedData.message) }
+//        return .success(tokenData.jwt)
+//        }
 //}
+//        
